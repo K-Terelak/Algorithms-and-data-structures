8a8cb9e10942e6af28b3a10360ca5fcf83860702
K-Terelak <k.terelak01@gmail.com>
2022-10-15 18:48:29 +0200

    added 2_5

--- lab_2/2_5/lab_2_5.py
+++ lab_2/2_5/lab_2_5.py
@@ -3,10 +3,10 @@ class Node:
     data = None
     next = None
 
-    def __init__(self, d, before=None, after=None):
+    def __init__(self, d, previous_node=None, next_node=None):
         self.data = d
-        self.previous = before
-        self.next = after
+        self.previous = previous_node
+        self.next = next_node
 
 
 class List:
@@ -15,53 +15,87 @@ class List:
 
     # util, return previous element from list
     def get_previous_node(self, i):
-        current = self.start
+        current_node = self.start
         for i in range(1, i):
-            current = current.next
-        return current
+            current_node = current_node.next
+        return current_node
 
     # add element to List with index
     def insert(self, item, i):
-
         # 1 insert node to the beginning of a ist
         if i == 0:
-            old_first = self.start
-            self.start = Node(item)
-            if self.len > 0:
-                self.start.next = old_first
+            old_start = self.start
+            newNode = Node(item, None, old_start)
+            if self.len > 0:  # insert node before existing elements
+                self.start = newNode
+                old_start.previous = newNode
+            else:  # add first node (list empty)
+                self.start = newNode
 
         # 2 insert node to the end of list
-        if i == self.len:
-            before = self.get_previous_node(self.len)
-            before.next = Node(before, item, None)
+        elif i == self.len:
+            previous_node = self.get_previous_node(self.len)
+            previous_node.next = Node(item, previous_node, None)
 
         # 3 insert node inside list
         elif i < self.len:
             before = self.get_previous_node(i)
-            after = before.next.next
-            after.previous = Node(item)
-            before.next = Node(item)
+            after = before.next
+            newNode = Node(item, before, after)
+            before.next, after.previous = newNode, newNode
         else:
             return IndexError("Index error")
         self.len += 1
 
-    #     TODO
+    def removeAt(self, i):
+        # 1  remove first node
+        if i == 0:
+            self.start = self.start.next
+            self.start.previous = None
+
+        # 2 remove last node
+        elif i == self.len - 1:
+            penultimate_node = self.get_previous_node(i)
+            penultimate_node.next = None
+
+        # remove node inside list
+        elif i < self.len:
+            previous_node = self.get_previous_node(i)
+            next_node = previous_node.next.next
+            previous_node.next, next_node.previous = next_node, previous_node
+        else:
+            return IndexError("Index error")
+        self.len -= 1
+
+    # get element at given index
+    def elemAt(self, i):
+        try:
+            current = self.start
+            for i in range(1, i + 1):
+                current = current.next
+            return current.data
+        except Exception:
+            return
 
     # print streamList (for testing)
     def print_list(self):
-        try:
-            print("\nPrint")
-            current_node = self.start
-            for i in range(0, self.len):
-                print(str(i) + " " + current_node.data)
-                current_node = current_node.next_node
-        except AttributeError:
-            return
+        print("\nStreamList:")
+        current_node = self.start
+        for i in range(0, self.len):
+            print(str(i) + " " + str(current_node.data))
+            current_node = current_node.next
 
 
 ls = List()
-ls.insert('a', 0)
-ls.insert('b', 1)
-ls.insert('c', 2)
-ls.insert('d', 3)
+ls.insert('b', 0)  # [b]
+ls.insert('a', 0)  # [a, b]
+ls.insert('d', 2)  # [a, b, d]
+ls.insert('c', 2)  # [a, b, c, d]
+ls.insert('e', 4)  # [a, b, c, d, e]
+ls.removeAt(0)  # [b, c, d, e]
+ls.removeAt(1)  # [b, d, e]
+print(ls.elemAt(0))  # b
+print(ls.elemAt(1))  # d
+print(ls.elemAt(2))  # e
+print(ls.elemAt(3))  # not found
 ls.print_list()
