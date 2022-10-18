9e5863d51026d4627a32db424a4fbaa914418cae
krzysztofterelak <k.terelak01@gmail.com>
2022-10-18 12:25:43 +0200

    added 7_2

---
+++ lab_7/7_2/lab_7_2.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [2, 1, 3, 7, 2, 1, 1, 5]
+bubble_sort(arr)
+print(arr)
